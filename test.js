fb722c0e2ea8b6514cb1525920a1954932a68b4c
tanxiqiao <1558184202@qq.com>
2018-11-17 11:02:15 +0800

    Changes to be committed: 	new file:   test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+function dig(obj,target){
+  for(var key in obj){
+    if(key===target){
+      console.log(obj[key]);
+    }
+    else{
+      if(obj[key] instanceof Object)
+      dig(obj[key],target);
+      else {
+        if(obj[key] instanceof Object)
+        dig(obj[key],target);
+        else{console.log(undefined);
+        }  
+      }
+    }
+  }
+}
